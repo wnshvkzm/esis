a4c45c89a975bbe44b13e72fbfcf2bfdf5ae69ce
wnshvkzm <swiftkal@e-sis.co.kr>
2016-12-27 13:06:01 +0900

    remove siteName

--- siteName/go/src/main/java/com/daou/go/cgmetal/dao/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package com.daou.go.cgmetal.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-	public Connection makeConnection()throws ClassNotFoundException, SQLException;
-}
--- siteName/go/src/main/java/com/daou/go/cgmetal/service/CgMailService.java
+++
@@ -1,7 +0,0 @@
-package com.daou.go.cgmetal.service;
-
-import java.sql.SQLException;
-
-public interface CgMailService {
-	public void vmailInsertValue(String sender, String receiver, String title, String content) throws SQLException;
-}
